f25c99cebc6a59b2eb04a1429d94395ae7b28622
Sashidhar Reddy Polu <sashidhar@rafay.co>
2026-02-08 01:13:37 +0530

    Updated endpoint

--- pkg/endpoint/init.go
+++ pkg/endpoint/init.go
@@ -5,6 +5,7 @@ import (
 	_ "github.com/k3s-io/kine/pkg/drivers/http"
 	_ "github.com/k3s-io/kine/pkg/drivers/mysql"
 	_ "github.com/k3s-io/kine/pkg/drivers/nats"
+	_ "github.com/k3s-io/kine/pkg/drivers/oxia"
 	_ "github.com/k3s-io/kine/pkg/drivers/pgsql"
 	_ "github.com/k3s-io/kine/pkg/drivers/sqlite"
 )
